5751b7cd73092f3ab14f256b45f1694501db0eec
Manoel Pedro <manoelpedromn@hotmail.com>
2025-05-18 09:38:41 -0300

    sunday

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-console.log("hello world")
-
-const form = document.getElementsByClassName("item")[0]
-const input = document.getElementById("item")
+const inputItem = document.getElementById("inputItem")
 const button = document.getElementById("button")
+const ul = document.getElementById("ul")
 
-function addList (){
-  console.log(input.value);
-  input.value = ""
-}
+const inputCheckBox = document.getElementById("checkbox")
+const buttonDelete = document.getElementsByClassName("btn-delete")
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault()
+button.addEventListener("click", function addItem(e) {
+  e.preventDefault()
+  let li = document.createElement("li")
+
+  let imageDelete = document.createElement("img")
+  imageDelete.src = "assets/icons/trash.svg"
+
+  let btnDelete = document.createElement("button")
+  btnDelete.classList.add("btn-delete")
+  btnDelete.appendChild(imageDelete)
+
+  let checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+
+  let span = document.createElement("span")
+  span.textContent = inputItem.value
+
+  li.classList.add("liStyle")
+  li.prepend(checkbox)
+  li.append(span)
+  li.append(btnDelete)
+
+  ul.appendChild(li)
+
+  inputItem.value = ""
 })
 
-button.addEventListener("click", addList)
+btnDelete.addEventListener("click", () => {})
